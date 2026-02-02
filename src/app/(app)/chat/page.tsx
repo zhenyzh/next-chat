34f6fe01723b9bd31a794885146277805917a55b
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-02-02 20:59:18 +0700

    +version and textfield

--- src/app/(app)/chat/page.tsx
+++ src/app/(app)/chat/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Box, Container } from "@zhenyzh/common-ui/components";
+import { Box, Container, TextField } from "@zhenyzh/common-ui/components";
 import { UserCardList } from "@/widgets/user-card-list";
 import { UserCard } from "@/features/user-card";
 import { MessageList } from "@/widgets/message-list";
+import { SendHorizontal, CirclePlus, Smile } from "lucide-react";
 import s from "./page.module.scss";
 
 export default function ChatPage() {
@@ -19,6 +20,26 @@ export default function ChatPage() {
       </Box>
       <Box className={s.contentMessage}>
         <MessageList />
+      </Box>
+      <Box className={s.sendMessage}>
+        <TextField
+          placeholder={"Сообщение"}
+          icon={
+            <CirclePlus
+              onClick={() => alert("иконка + ")}
+              className={s.point}
+            />
+          }
+          iconEnd={
+            <>
+              <Smile className={s.point} style={{ marginRight: "20px" }} />
+              <SendHorizontal
+                onClick={() => alert("иконка send ")}
+                className={s.point}
+              />
+            </>
+          }
+        />
       </Box>
     </Container>
   );
