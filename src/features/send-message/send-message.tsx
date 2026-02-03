cab127dad9722a9b3af28a75111e2f1e49423117
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-02-03 14:23:09 +0700

    fix

--- src/features/send-message/send-message.tsx
+++ src/features/send-message/send-message.tsx
@@ -1,13 +1,13 @@
 "use client";
 
+import { useState } from "react";
+
 import { TextField } from "@zhenyzh/common-ui/components";
 
 import { Add } from "./ui/add";
 import { EmojiSmile } from "./ui/emoji-smile";
 import { Sending } from "./ui/sending";
 import { Microphone } from "./ui/microphone";
-import s from "./send-message.module.scss";
-import { useState } from "react";
 
 export function SendMessage() {
   const [message, setMessage] = useState("");
