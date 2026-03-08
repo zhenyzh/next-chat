fdbf220a52097636026b543b5f0efe12beeec0b1
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-08 16:38:33 +0700

    settings from query

--- src/shared/query-client/query-client.ts
+++ src/shared/query-client/query-client.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1 * 60 * 1000,
+      gcTime: 24 * 60 * 60 * 1000,
+    },
+  },
+});
