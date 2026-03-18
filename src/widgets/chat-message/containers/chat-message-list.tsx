6a453d0e759219f26ee2902b857ead538b0e8a6d
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-18 12:52:50 +0700

    fix

--- src/widgets/chat-message/containers/chat-message-list.tsx
+++ src/widgets/chat-message/containers/chat-message-list.tsx
@@ -2,20 +2,17 @@ import { SubDateContent } from "../ui/sub-time-content";
 import { MessageListItem } from "../ui/message-list-item";
 import { EmptyContent } from "../ui/empty-content";
 import { useGetMessages, useScrollToBottom } from "../model/hooks";
-import { MessagesSkeleton } from "@/entities/messages";
 import { List, ScrollBar } from "@/shared/ui";
 import s from "./chat-message-list.module.scss";
 
 export function ChatMessageList() {
   const { scrollRef } = useScrollToBottom();
-  const { messages, hasChatId, isLoading } = useGetMessages();
+  const { messages, hasChatId } = useGetMessages();
 
   return (
     <ScrollBar ref={scrollRef}>
       <List
         data={messages}
-        isLoading={isLoading}
-        skeleton={<MessagesSkeleton />}
         className={s.container}
         renderItem={(group) => (
           <>
