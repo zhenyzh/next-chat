25a9ec59483257f3f3717efd63e08e434f48889a
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-27 13:40:56 +0700

    refact: scroll to bottom

--- src/widgets/chat-message/model/hooks/use-scroll-to-bottom.ts
+++ src/widgets/chat-message/model/hooks/use-scroll-to-bottom.ts
@@ -8,39 +8,39 @@ export function useScrollToBottom() {
   const { messages } = useChatMessage();
   const { chatId } = useChatOpenCacheQuery();
   const typingUsersIds = useTypingUsersIds();
-
   const { ref: refWatchBottom, isBottom } = useBottomObserver({
-    dependencies: [messages.length, typingUsersIds.length, chatId],
+    dependencies: [messages.length, chatId],
   });
 
   const scrollRef = useRef<HTMLDivElement>(null);
-  const isFirstRender = useRef(true);
+
+  const scrollToBottom = () => {
+    const el = scrollRef.current;
+    if (!el) return;
+    el.scrollTop = el.scrollHeight;
+  };
 
   useEffect(() => {
-    isFirstRender.current = true;
+    scrollToBottom();
   }, [chatId]);
 
   useEffect(() => {
-    const el = scrollRef.current;
-    if (!el) return;
-
-    if (isFirstRender.current) {
-      el.scrollTop = el.scrollHeight;
-      isFirstRender.current = false;
-      return;
-    }
-
     if (!messages.length) return;
 
     const lastGroup = messages[messages.length - 1].messages;
-    const lastMessage = lastGroup[lastGroup.length - 1];
+    const isLastFromMe = lastGroup[lastGroup.length - 1].fromMe;
 
-    const isMe = lastMessage.fromMe;
-
-    if (isMe || isBottom) {
-      el.scrollTop = el.scrollHeight;
+    if (isLastFromMe || isBottom) {
+      scrollToBottom();
     }
-  }, [messages, typingUsersIds, isBottom, chatId]);
+  }, [messages, isBottom, chatId]);
+
+  // находимся в конце, под скроллим новые сообщения
+  useEffect(() => {
+    if (isBottom) {
+      scrollToBottom();
+    }
+  }, [typingUsersIds.length, isBottom]);
 
   return { scrollRef, refWatchBottom, isBottom };
 }
