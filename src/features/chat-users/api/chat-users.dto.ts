8e689eea3020d89b735f78170076224243f4a7d4
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-03-30 15:30:18 +0700

    fix

--- src/features/chat-users/api/chat-users.dto.ts
+++ src/features/chat-users/api/chat-users.dto.ts
@@ -1,9 +1,9 @@
 import type { User } from "@/entities/user/model/types";
 import type { StatusMessage } from "@/entities/messages";
 
-export type ChatUsersDto = Omit<User, "email"> &
-  StatusMessage & {
-    createdAt: string;
-    lastMessage: string;
-    countUnreadMessage: number;
-  };
+export type ChatUsersDto = {
+  createdAt: string;
+  lastMessage: string;
+  countUnreadMessage: number;
+} & Omit<User, "email"> &
+  StatusMessage;
