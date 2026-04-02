3629aed91de42edb9da0a56a7c5e4acb9c1e0b66
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-04-02 17:00:57 +0700

    fix: import

--- src/features/add-user-avatar/lib/utils/update-query-data.ts
+++ src/features/add-user-avatar/lib/utils/update-query-data.ts
@@ -1,6 +1,6 @@
-import { queryClient } from "@/shared/api";
 import type { MessagesDto } from "@/entities/messages/api";
 import type { User } from "@/entities/user/model/types";
+import { queryClient } from "@/shared/api";
 
 export function updateUserData<K>(queryKey: K[], avatarUrl: string) {
   queryClient.setQueryData<User>(queryKey, (old) =>
