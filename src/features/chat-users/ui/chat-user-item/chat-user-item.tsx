586df1219d97f4c0af69ce7c85652180127c6616
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-04-03 09:55:56 +0700

    fix

--- src/features/chat-users/ui/chat-user-item/chat-user-item.tsx
+++ src/features/chat-users/ui/chat-user-item/chat-user-item.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@zhenyzh/common-ui/components";
 import { ChatSubContent } from "../chat-sub-content";
 import type { ChatUsers } from "../../model/types";
 import { useHasUserStatus } from "@/features/users-status/model/hooks";
@@ -8,19 +9,21 @@ export function ChatUserItem({ data }: { data: ChatUsers }) {
   const isOnline = useHasUserStatus(data.id);
 
   return (
-    <UserPreview
-      name={data.name}
-      avatarUrl={data?.avatarUrl}
-      isOnline={isOnline}
-      rightInfoSlot={<DateTime value={formatDateTimeAgo(data.createdAt)} />}
-      subInfoSlot={
-        <ChatSubContent
-          message={data.lastMessage}
-          status={data.status}
-          countMessage={data.countUnreadMessage}
-          typedI={data.typedI}
-        />
-      }
-    />
+    <Box style={{ "--avatar-online-bg": "var(--color-bg-primary-v1)" }}>
+      <UserPreview
+        name={data.name}
+        avatarUrl={data?.avatarUrl}
+        isOnline={isOnline}
+        rightInfoSlot={<DateTime value={formatDateTimeAgo(data.createdAt)} />}
+        subInfoSlot={
+          <ChatSubContent
+            message={data.lastMessage}
+            status={data.status}
+            countMessage={data.countUnreadMessage}
+            typedI={data.typedI}
+          />
+        }
+      />
+    </Box>
   );
 }
