c1cc72823433fec00296a123aa6cd41fe9dad827
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-04-07 16:10:03 +0700

    fix: use open chat

--- src/entities/chat/model/hooks/use-open-chat.ts
+++ src/entities/chat/model/hooks/use-open-chat.ts
@@ -1,25 +1,9 @@
 import { useMutation } from "@tanstack/react-query";
 import { chatApi } from "@/entities/chat/api";
-import { useSearchQueryParams } from "@/shared/hooks";
-import { queryClient } from "@/shared/api";
 
 export function useOpenChat() {
-  const {
-    query: { recipientId },
-  } = useSearchQueryParams();
-
-  const queryKey = [chatApi.baseKey, recipientId];
-
   const mutation = useMutation({
     mutationFn: chatApi.chatOpen,
-
-    async onSettled() {
-      queryClient.invalidateQueries({ queryKey });
-    },
-
-    async onSuccess(data) {
-      queryClient.setQueryData(queryKey, data);
-    },
   });
 
   const handleChatOpen = (recipientId: number) => {
