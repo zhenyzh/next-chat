a4a4311b50b52b22828cebb098812e9ce02c711c
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-04-20 15:58:07 +0700

    fix: namespace

--- src/widgets/message-input/conteiners/message-input.tsx
+++ src/widgets/message-input/conteiners/message-input.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@zhenyzh/common-ui/components";
 import { FileAttachList } from "../ui/file-attach-list";
-import { MessageInputField } from "../ui/message-field";
+import { MessageInputField } from "../ui/message-input-field";
 import { useMessageFieldAutoHeight } from "@/features/message-field-height/model/hooks";
 
 export function MessageInput() {
