3ad1b0d50a9508b264cabda6692b6a0c42507c3c
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-04-23 09:33:42 +0700

    add link

--- src/entities/messages/ui/message-text/message-text.tsx
+++ src/entities/messages/ui/message-text/message-text.tsx
@@ -1,10 +1,17 @@
-import { Typography } from "@zhenyzh/common-ui/components";
+import Linkify from "react-linkify";
+import { Box, Typography } from "@zhenyzh/common-ui/components";
 import s from "./message-text.module.scss";
 
 export function MessageText({ text }: { text: string }) {
+  const decorator = (href: string, text: string, key: number) => (
+    <Box as="a" key={key} href={href} className={s.link}>
+      {text}
+    </Box>
+  );
+
   return (
     <Typography variant="h3" className={s.text}>
-      {text}
+      <Linkify componentDecorator={decorator}>{text}</Linkify>
     </Typography>
   );
 }
