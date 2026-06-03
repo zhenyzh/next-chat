d6363611f61d1e917f241b41bff9a6b6d31fb83b
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-06-03 15:39:33 +0700

    delete test search chat users

--- src/features/search-chat-users/containers/search-chat-users.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { SearchChatUsers } from "./search-chat-users";
-
-it("Рендер поля поиска", () => {
-  render(<SearchChatUsers />);
-  expect(screen.getByPlaceholderText("Поиск")).toBeInTheDocument();
-});
