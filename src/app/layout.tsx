20e3310e9329de8aa0f50ece6da8757fc5c6bf17
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-06-10 12:06:33 +0700

    add logo in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,14 @@ import type { Metadata } from "next";
 import "@zhenyzh/common-ui/styles";
 import "./themas/global.scss";
 import Provider from "./providers";
+import Logo from "@/shared/assets/images/logo.svg";
 
 export const metadata: Metadata = {
   title: "Next chat",
   description: "next app chat",
+  icons: {
+    icon: Logo.src,
+  },
 };
 
 export default function RootLayout({
